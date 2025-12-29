7e072825edf8ea6335930716411b647ca604de4f
efe <168096049+visionis@users.noreply.github.com>
2025-12-29 22:40:01 +0300

    Update package configuration in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/visionis/TaskFlow",
-    packages=find_packages(),
+        packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    
     include_package_data=True,
     install_requires=[],
     python_requires='>=3.10',
